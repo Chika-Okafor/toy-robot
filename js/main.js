36194e50a9f48134788a5090bb41f6af6866e7eb
Chika_Okafor <chikamichaelokafor@gmail.com>
2022-03-17 00:48:41 +0000

    added left and right controls to move the robot

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ const rowInput = document.getElementById("row");
 const columnInput = document.getElementById("column");
 const facingInput = document.getElementById("facing");
 const submit = document.getElementById("submit");
+const rotateLeftBtn = document.getElementById("rotate-left-btn");
+const rotateRightBtn = document.getElementById("rotate-right-btn");
+const moveBtn = document.getElementById("move-btn");
 
 
-
+let robotFacingDirection = "";
 
 //ENABLES SELECT ELEMENT FOR FACING DIRECTION WHEN PLACING A ROBOT ON THE BOARD
 placedItem.onchange = function () {
@@ -23,14 +26,14 @@ submit.addEventListener("click", function (e) {
     e.preventDefault();
     const selectedRow = rowInput.value;
     const selectedColumn = columnInput.value;
-    const selectedDirection = facingInput.value;
+    robotFacingDirection = facingInput.value;
     
     if (placedItem.value === "ROBOT") {
-        placeRobot(selectedRow, selectedColumn, selectedDirection);
+        placeRobot(selectedRow, selectedColumn, robotFacingDirection);
         
     } else if (placedItem.value === "WALL") {
         placeWall(selectedRow, selectedColumn)
-    }
+    };
 });
 
 
@@ -45,6 +48,7 @@ const placeRobot = (row, column, facing) => {
         console.log(newRobot);
         newRobot.classList.replace("empty-cell", "robot");
         newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`;
+        console.log(robotFacingDirection);
         robotPosition = newRobot;
     } else if (robotPosition !== null) {
         newRobot = document.querySelector(`${rowID} ${cellID}`);
@@ -57,6 +61,7 @@ const placeRobot = (row, column, facing) => {
             robotPosition.innerHTML = "";
             newRobot.classList.replace("empty-cell", "robot");
             newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`;
+            console.log(robotFacingDirection);
             robotPosition = newRobot;
         };
     }
@@ -80,22 +85,92 @@ const placeWall = (row, column) => {
 };
 
 
+rotateLeftBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    const direction = document.getElementById("direction");
+
+    if (direction !== null) {
+        if (robotFacingDirection === "NORTH") {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "WEST";
+            direction.classList.replace("fa-arrow-up", "fa-arrow-left");
+            console.log(robotFacingDirection);
+        } else if (robotFacingDirection === "EAST") {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "NORTH";
+            direction.classList.replace("fa-arrow-right", "fa-arrow-up");
+            console.log(robotFacingDirection);
+        } else if (robotFacingDirection === "SOUTH") {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "EAST";
+            direction.classList.replace("fa-arrow-down", "fa-arrow-right");
+            console.log(robotFacingDirection);
+        } else {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "SOUTH";
+            direction.classList.replace("fa-arrow-left", "fa-arrow-down");
+            console.log(robotFacingDirection);
+        };
+    };
+    
+    
+});
+
+rotateRightBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    const direction = document.getElementById("direction");
+
+    if (direction !== null) {
+        if (robotFacingDirection === "NORTH") {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "EAST";
+            direction.classList.replace("fa-arrow-up", "fa-arrow-right");
+            console.log(robotFacingDirection);
+        } else if (robotFacingDirection === "EAST") {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "SOUTH";
+            direction.classList.replace("fa-arrow-right", "fa-arrow-down");
+            console.log(robotFacingDirection);
+        } else if (robotFacingDirection === "SOUTH") {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "WEST";
+            direction.classList.replace("fa-arrow-down", "fa-arrow-left");
+            console.log(robotFacingDirection);
+        } else {
+            console.log(robotFacingDirection);
+            robotFacingDirection = "NORTH";
+            direction.classList.replace("fa-arrow-left", "fa-arrow-up");
+            console.log(robotFacingDirection);
+        };
+    };
+    
+    
+});
+
+
+
+    
+    
+});
+
 
 
 
 const setFacingDirection = facing => {
     let robotIcon = "";
     if (facing === "NORTH") {
-        robotIcon += "<i class='fa-solid fa-arrow-up'></i>";
+        robotIcon += "<i class='fa-solid fa-arrow-up' id='direction'></i>";
         console.log(robotIcon);
     } else if (facing === "EAST") {
-        robotIcon += "<i class='fa-solid fa-arrow-right'></i>";
+        robotIcon += "<i class='fa-solid fa-arrow-right' id='direction'></i>";
         console.log(robotIcon);
     } else if (facing === "SOUTH") {
-        robotIcon += "<i class='fa-solid fa-arrow-down'></i>";
+        robotIcon += "<i class='fa-solid fa-arrow-down' id='direction'></i>";
         console.log(robotIcon);
     } else {
-        robotIcon += "<i class='fa-solid fa-arrow-left'></i>";
+        robotIcon += "<i class='fa-solid fa-arrow-left' id='direction'></i>";
         console.log(robotIcon);
     };
     return robotIcon;
