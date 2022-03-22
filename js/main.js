142b24cf3155ab1b77392f274d9dd54e917dd177
Chika_Okafor <chikamichaelokafor@gmail.com>
2022-03-22 13:35:40 +0000

    added comments for variables in the main.js file

--- js/main.js
+++ js/main.js
@@ -68,13 +68,13 @@ const numberOfColumns = 5; //NUMBER OF COLUMNS ON THE BOARD
 const numberOfCells = numberOfRows * numberOfColumns; //CALCULATES TOTAL CELLS ON THE BOARD GIVEN NUMBER OF ROWS AND COLUMNS
 
 //BOUNDARIES FOR ROWS AND COLUMNS
-const xBoundary = getBoundary(1, numberOfRows); //GENERATES A RANGE OF NUMBERS FROM 1 TO THE TOTAL NUMBER OF ROWS ON THE BOARD
-const yBoundary = getBoundary(1, numberOfCells); // GENERATES A RANGE OF NUMBERS FROM 1 TO TOTAL NUMBER OF CELLS IN ALL THE ROWS
+const xBoundary = getBoundary(1, numberOfRows);
+const yBoundary = getBoundary(1, numberOfCells);
 
-const yEastBoundary = getFacingBoundary(numberOfColumns, numberOfCells, numberOfColumns); //GENERATES A LIST OF NUMBERS ON THE EAST-SIDE BOUNDARY OF THE BOARD
-const yWestBoundary = getFacingBoundary(1, ((numberOfCells - numberOfColumns) + 1), 5); //GENERATES A LIST OF NUMBERS ON THE WEST-SIDE BOUNDARY OF THE BOARD
-const yNorthBoundary = yBoundary.slice((numberOfCells - numberOfColumns), numberOfCells); //GENERATES A LIST OF NUMBERS ON THE NORTH-SIDE BOUNDARY OF THE BOARD
-const ySouthBoundary = yBoundary.slice(0, numberOfColumns); //GENERATES A LIST OF NUMBERS ON THE SOUTH-SIDE BOUNDARY OF THE BOARD
+const yEastBoundary = getFacingBoundary(numberOfColumns, numberOfCells, numberOfColumns); //EASTERN BOUNDARY CELL IDS
+const yWestBoundary = getFacingBoundary(1, ((numberOfCells - numberOfColumns) + 1), 5); //WESTERN BOUNDARY CELL IDS
+const yNorthBoundary = yBoundary.slice((numberOfCells - numberOfColumns), numberOfCells); //NORTHERN BOUNDARY CELL IDS
+const ySouthBoundary = yBoundary.slice(0, numberOfColumns); //SOUTHERN BOUNDARY CELL IDS
 
 
 /********************* FACTORY FUNCTION FOR OBJECTS (ROBOT AND WALL) ***********************/
@@ -97,14 +97,14 @@ const Wall = CreateItem("Wall");
 /********************************** GAME MECHANICS ********************************************/
 //DISABLES FACING DIRECTION WHEN A WALL IS BEING PLACED ON THE BOARD
 placedObject.onchange = function () {
-    facingInput.setAttribute("disabled", "disabled"); //DISABLE FACING DIRECTION SELECT ELEMENT
-    if (this.value === Robot.type) { //IF PLACED OBJECT IS A ROBOT:
-        facingInput.removeAttribute("disabled"); //ENABLE FACING DIRECTION SELECT ELEMENT
+    facingInput.setAttribute("disabled", "disabled");
+    if (this.value === Robot.type) {
+        facingInput.removeAttribute("disabled");
     };
 };
 
 
-/*********** GAME FUNCTIONS ***********/
+/*********************************************** GAME FUNCTIONS *********************************/
 
 //SETS PROPERTY VALUES FOR ROBOT AND WALL OBJECTS FROM HTML SELECT ELEMENT VALUES
 const setItemValues = (itemObject) => {
@@ -122,84 +122,75 @@ const setIDs = (row, column) => {
 //TOGGLES THE ARROW ICON TO SHOW ROBOT'S FACING DIRECTION AT ANY INSTANT
 const setFacingDirection = (facing) => {
     let icons = "";
-    if (facing === "North") { //IF ROBOT OBJECT FACES NORTH:
-        icons += "<i class='fa-solid fa-arrow-up' id='direction'></i>"; //SET ARROW ICON TO FACE NORTH
-    } else if (facing === "East") { //ELSE IF ROBOT OBJECT FACES EAST:
-        icons += "<i class='fa-solid fa-arrow-right' id='direction'></i>"; //SET ARROW ICON TO FACE EAST
-    } else if (facing === "South") { //ELSE IF ROBOT OBJECT FACES SOUTH:
-        icons += "<i class='fa-solid fa-arrow-down' id='direction'></i>"; //SET ARROW ICON TO FACE SOUTH
-    } else { //ELSE IF ROBOT OBJECT FACES WEST:
-        icons += "<i class='fa-solid fa-arrow-left' id='direction'></i>"; //SET ARROW ICON TO FACE WEST
+    if (facing === "North") {
+        icons += "<i class='fa-solid fa-arrow-up' id='direction'></i>";
+    } else if (facing === "East") {
+        icons += "<i class='fa-solid fa-arrow-right' id='direction'></i>";
+    } else if (facing === "South") {
+        icons += "<i class='fa-solid fa-arrow-down' id='direction'></i>";
+    } else {
+        icons += "<i class='fa-solid fa-arrow-left' id='direction'></i>";
     };
     return icons;
 };
 
 //CREATES A NEW ROBOT IN AN EMPTY CELL
 const createNewRobot = (facing) => {
-    newRobot = document.querySelector(cellID); //SET THE PROPECTIVE CELL WHERE THE ROBOT IS TO BE PLACED
+    newRobot = document.querySelector(cellID);
 
-    if (newRobot.getAttribute("class") === "empty-cell") { //IF THE PROSPECTIVE CELL IS EMPTY:
-        newRobot.classList.replace("empty-cell", "robot"); //ADD A ROBOT TO THE PROSPECTIVE CELL
-        newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`; //SET THE FACING DIRECTION OF THE ROBOT
-        robotPosition = newRobot; //SET THE PROSPECTIVE CELL POSITION AS THE ROBOT'S CURRENT POSITION
-        message.innerHTML = `Android001 at your service!`; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
-    } else if (newRobot.getAttribute("class") === "wall") { //ELSE IF THERE IS A WALL POSITIONED IN THE CELL:
-        message.innerHTML = "Are you seriously trying to place me on a wall?!"; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
+    if (newRobot.getAttribute("class") === "empty-cell") {
+        newRobot.classList.replace("empty-cell", "robot");
+        newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`;
+        robotPosition = newRobot;
+        message.innerHTML = `Android001 at your service!`;
+    } else if (newRobot.getAttribute("class") === "wall") {
+        message.innerHTML = "Are you seriously trying to place me on a wall?!";
     };
 };
 
 //MOVES EXISTING ROBOT TO AN EMPTY CELL
 const moveToNewCell = (facing) => {
-    
-    newRobot = document.querySelector(cellID); //SET THE PROPECTIVE CELL WHERE ROBOT IS TO BE MOVED
+    newRobot = document.querySelector(cellID);
 
-    
-    if (newRobot.getAttribute("class") === "empty-cell") { //IF THE PROSPECTIVE CELL IS EMPTY:
-        
-        if (robotPosition !== newRobot) { //AND IF THE ROBOT'S CURRENT POSITION IS NOT THE SAME AS THE PROSPECTIVE CELL:
-            //REMOVE THE ROBOT FROM IT'S CURRENT POSITION
+    if (newRobot.getAttribute("class") === "empty-cell") {
+        if (robotPosition !== newRobot) {
             robotPosition.innerHTML = "";
             robotPosition.classList.replace("robot", "empty-cell");
         };
-        newRobot.classList.replace("empty-cell", "robot"); //THEN PLACE THE ROBOT IN THE PROSPECTIVE CELL
-        newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`; //SET THE FACING DIRECTION OF THE ROBOT
-        robotPosition = newRobot; //SET THE PROSPECTIVE CELL POSITION AS THE ROBOT'S CURRENT POSITION
-    } else if (newRobot.getAttribute("class") === "robot") { //ELSE IF THERE IS ALREADY A ROBOT IN THE PROSPECTIVE CELL:
-        newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`; //UPDATE THE ROBOT'S FACING DIRECTION
-        message.innerHTML = `Confirming new facing direction...<br />`; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
-    } else if (newRobot.getAttribute("class") === "wall") { //ELSE IF THERE IS A WALL IN THE PROSPECTIVE CELL:
-        message.innerHTML = "Ran into a wall. Just great! <br />"; ////OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
+        newRobot.classList.replace("empty-cell", "robot");
+        newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`;
+        robotPosition = newRobot;
+    } else if (newRobot.getAttribute("class") === "robot") {
+        newRobot.innerHTML = `<i class='fa-solid fa-robot fa-xl'></i>${setFacingDirection(facing)}`;
+        message.innerHTML = `Confirming new facing direction...<br />`;
+    } else if (newRobot.getAttribute("class") === "wall") {
+        message.innerHTML = "Ran into a wall. Just great! <br />";
     };
-    //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
     message.innerHTML += `Android in position. Awaiting new orders!`;
 };
 
 //HANDLES PLACING OF ROBOT OBJECT ON THE BOARD
 const placeRobot = (row, column, facing) => {
-    setIDs(row, column); //SET THE SPECIFIC ROW AND CELL ID OF THE PROSPECTIVE CELL
+    setIDs(row, column);
     
-    if (robotPosition === null) { //IF THERE IS NO ROBOT ON THE BOARD:
-        createNewRobot(facing); //CREATE A NEW ROBOT
-    } else { //ELSE:
-        moveToNewCell(facing); //MOVE THE EXISTING ROBOT TO THE PROSPECTIVE CELL
+    if (robotPosition === null) {
+        createNewRobot(facing);
+    } else {
+        moveToNewCell(facing);
     };
 };
 
 //HANDLES PLACING OF WALL OBJECTS ON THE BOARD
 const placeWall = (row, column) => {
-    setIDs(row, column); //SET THE SPECIFIC ROW AND CELL ID OF THE PROSPECTIVE CELL
+    setIDs(row, column);
+    const newWall = document.querySelector(cellID);
 
-    const newWall = document.querySelector(cellID); //SET THE PROPECTIVE CELL WHERE THE WALL IS TO BE PLACED
-
-    if (newWall.getAttribute("class") === "empty-cell") { //IF THE PROSPECTIVE CELL IS EMPTY:
-        newWall.classList.replace("empty-cell", "wall"); //PLACE A WALL IN THE CELL
-    } else if (newWall.getAttribute("class") === "robot") { //ELSE IF THE IS A ROBOT IN THE CELL:
-        message.innerHTML = `Row ${row}, column ${column} is not empty!`; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
+    if (newWall.getAttribute("class") === "empty-cell") {
+        newWall.classList.replace("empty-cell", "wall");
+    } else if (newWall.getAttribute("class") === "robot") {
+        message.innerHTML = `Row ${row}, column ${column} is not empty!`;
     };
-    
-    //ALSO, IF THERE IS A ROBOT ON THE BOARD:
     if (robotPosition !== null) {
-        //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
         message.innerHTML = `Detecting a new wall at row ${row}, column ${column}!`;
     };
 };
@@ -215,56 +206,55 @@ const resetElementValues = () => {
 
 //ROTATES ROBOT 90 DEGREES TO A GIVEN DIRECTION
 const rotateRobot = (rotation) => {
-    message.innerHTML = ""; //EMPTY THE MESSAGE TERMINAL
-    const direction = document.getElementById("direction"); //SET THE ROBOT'S FACING DIRECTION HTML PARTICULARS
-    
-    if (direction !== null) { //IF THE ROBOT HAS FACING DIRECTION INFORMATION IN THE HTML:
-        if (rotation === "Left") { //AND IF THE TARGET DESTINATION IS LEFT:
-            if (Robot.facing === "North") { //AND THE ROBOT IS FACING NORTH:
-                Robot.facing = "West"; //SET THE ROBOT TO FACE WEST
-                direction.classList.replace("fa-arrow-up", "fa-arrow-left"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
-            } else if (Robot.facing === "East") { //ELSE IF THE ROBOT IS FACING EAST
-                Robot.facing = "North"; //SET THE ROBOT TO FACE NORTH
+    message.innerHTML = "";
+    const direction = document.getElementById("direction");
+
+    if (direction !== null) {
+        if (rotation === "Left") {
+            if (Robot.facing === "North") {
+                Robot.facing = "West";
+                direction.classList.replace("fa-arrow-up", "fa-arrow-left");
+            } else if (Robot.facing === "East") {
+                Robot.facing = "North";
                 direction.classList.replace("fa-arrow-right", "fa-arrow-up");
-            } else if (Robot.facing === "South") { //ELSE IF THE ROBOT IS FACING SOUTH
-                Robot.facing = "East"; //SET THE ROBOT TO FACE EAST
-                direction.classList.replace("fa-arrow-down", "fa-arrow-right"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
-            } else { //ELSE IF THE ROBOT IS FACING
-                Robot.facing = "South"; //SET THE ROBOT TO FACE SOUTH
-                direction.classList.replace("fa-arrow-left", "fa-arrow-down"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
+            } else if (Robot.facing === "South") {
+                Robot.facing = "East";
+                direction.classList.replace("fa-arrow-down", "fa-arrow-right");
+            } else {
+                Robot.facing = "South";
+                direction.classList.replace("fa-arrow-left", "fa-arrow-down");
             };
-            message.innerHTML = `Affirmative! Turning left...`; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
-        } else if (rotation === "Right") { //ELSE IF THE TARGET DESTINATION IS LEFT:
-            
-            if (Robot.facing === "North") { //ELSE IF THE ROBOT IS FACING NORTH
-                Robot.facing = "East"; //SET THE ROBOT TO FACE EAST
-                direction.classList.replace("fa-arrow-up", "fa-arrow-right"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
-            } else if (Robot.facing === "East") { //ELSE IF THE ROBOT IS FACING EAST
-                Robot.facing = "South"; //SET THE ROBOT TO FACE SOUTH
-                direction.classList.replace("fa-arrow-right", "fa-arrow-down"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
-            } else if (Robot.facing === "South") { //ELSE IF THE ROBOT IS FACINGSOUTH
-                Robot.facing = "West"; //SET THE ROBOT TO FACE WEST
-                direction.classList.replace("fa-arrow-down", "fa-arrow-left"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
-            } else { //ELSE IF THE ROBOT IS FACING WEST
-                Robot.facing = "North"; //SET THE ROBOT TO FACE NORTH
-                direction.classList.replace("fa-arrow-left", "fa-arrow-up"); //UPDATE ITS HTML PARTICULARS TO REFLECT THE CHANGE
+            message.innerHTML = `Affirmative! Turning left...`;
+        } else if (rotation === "Right") {
+            if (Robot.facing === "North") {
+                Robot.facing = "East";
+                direction.classList.replace("fa-arrow-up", "fa-arrow-right");
+            } else if (Robot.facing === "East") {
+                Robot.facing = "South";
+                direction.classList.replace("fa-arrow-right", "fa-arrow-down");
+            } else if (Robot.facing === "South") {
+                Robot.facing = "West";
+                direction.classList.replace("fa-arrow-down", "fa-arrow-left");
+            } else {
+                Robot.facing = "North";
+                direction.classList.replace("fa-arrow-left", "fa-arrow-up");
             };
-            message.innerHTML = `Affirmative! Turning right...`; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
+            message.innerHTML = `Affirmative! Turning right...`;
         };
     };
 };
 
 //MOVES THE ROBOT ONE STEP TOWARDS ITS FACING DIRECTION
 const moveRobot = (row, column) => {
-    rowID = "#row-" + row; //SET ROW ID FOR THE PROSPECTIVE CELL
-    cellID = "#cell-" + column; //SET COLUMNID FOR THE PREOSPECTIVE CELL
-    message.innerHTML = `New coordinates confirmed!<br />`; //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
+    rowID = "#row-" + row;
+    cellID = "#cell-" + column;
+    message.innerHTML = `New coordinates confirmed!<br />`;
 
-    if (robotPosition !== null) { //IF THERE ARE NO ROBOTS ON THE BOARD:
-        if (newRobot === null) { //AND IF A PROSPECTIVE CELL HAS NOT BEEN SET
-            newRobot = document.querySelector(cellID); //SET THE PROSPECTIVE CELL WITH THE GIVEN ID
+    if (robotPosition !== null) {
+        if (newRobot === null) {
+            newRobot = document.querySelector(cellID);
         };
-        moveToNewCell(Robot.facing); //MOVE ROBOT TO THE PROSPECTIVE CELL
+        moveToNewCell(Robot.facing);
     };
 };
 
@@ -334,7 +324,6 @@ submit.addEventListener("click", function (e) {
             setItemValues(Robot);
             placeRobot(Robot.row, Robot.column, Robot.facing);
         } else {
-            //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
             message.innerHTML = "Warning: Data incomplete! Provide valid data!!";
         };
     } else if (placedObject.value === Wall.type) {
@@ -342,7 +331,6 @@ submit.addEventListener("click", function (e) {
             setItemValues(Wall);
             placeWall(Wall.row, Wall.column)
         } else {
-        //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
         message.innerHTML = "Oops! And the walls came Tumbling down!!!";
         };
     };
@@ -376,6 +364,5 @@ reportBtn.addEventListener("click", function (e) {
     let xCurrentPosition = robotPosition.parentElement.getAttribute("id")[4];
     let yCurrentPosition = robotPosition.getAttribute("id").slice(5);
     const columNumber = (Number(yCurrentPosition) - ((Number(xCurrentPosition) - 1) * 5));
-    //OUTPUT THIS MESSAGE FOR THE USER IN THE TERMINAL
     message.innerHTML = `ROUTINE REPORT: Android is positioned at ${xCurrentPosition}, ${columNumber} ${Robot.facing}`;
 });
